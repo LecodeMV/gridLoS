c6b426735a7001a2f51d29826c783a578a658cbd
Lecode <lecodeworksgame@gmail.com>
2018-04-24 23:41:28 +0100

    Done

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # gridLoS
-Line of Sight builder for a targeting system
+
+Line of Sight builder for targeting systems. Not so much intended for computing visibility but for detecting which cells are targetable or not.
+
+See demo.
+
+## Installation
+
+With npm:
+
+    npm install gridLoS
+  
+In the browser:
+
+    <script src="gridLoS.js"></script>
+The variable `gridLoS` will be exposed. 
+
+## Usage
+
+    let gridLoS = require('gridLoS');
+
+    let userCell = {x: 0, y: 0};
+    let cells = [
+        {x: 0, y: 0},
+        {x: 1, y: 0, block: true},
+        {x: 2, y: 0},
+        {x: 0, y: 1},
+        {x: 1, y: 1},
+        {x: 2, y: 1},
+    ];
+    gridLoS.make(cells, userCell, 2);
+
+`gridLoS.make` process the given cells and alter the property `visible`, marking if their are targetable or not.
+
+### Settings
+
+There are some settings you can alter before computing the cells.
+
+    gridLoS.settings({
+        cellObstacleProp: "block",
+        cellVisibleProp: "visible",
+        cellWidth: 32,
+        cellHeight: 32
+    });
+
+### From Maps
+
+    let map = [
+        [0,1,0],
+        [0,0,0],
+        [0,0,0]
+    ];
+    let userCell = {x: 0, y: 0};
+    let cells = gridLoS.mapToCells(map);
+    gridLoS.make(cells, userCell, 2);
+
+### Functions
+
+* `settings({})`  
+
+* `make(cells, playerPosition, range)`  
+`range` being arbritarily how far the player should be able to "see" the cells.  
+Return value: `{ intersectingCells, cellsInShadow, polylines, cells }`
+
+* `mapToCells([])`  
+Convert a readable map of cells (0: passable, 1: obstacle) to cell objects.
---
+++ demo/main.js
@@ -0,0 +1,107 @@
+window.onload = function () {
+    let cellW = 28, cellH = 28;
+    gridLoS.settings({
+        cellVisibleProp: "isVisible",
+        cellObstacleProp: "isObstacle",
+        cellWidth: cellW,
+        cellHeight: cellH
+    });
+
+    let map = [
+        //                             @
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], //@
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    let user = { x: 10, y: 10 };
+    let showPolygons = false;
+
+    function draw() {
+        let cells = gridLoS.mapToCells(map);
+        let result = gridLoS.make(cells, user, 20);
+        let polylines = result.polylines;
+        let intersectingCells = result.intersectingCells;
+        let cellsInShadow = result.cellsInShadow;
+
+        var canvas = document.getElementById("canvas");
+        canvas.width = cellW * map[0].length;
+        canvas.height = cellH * map.length;
+
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        cells.forEach(c => {
+            ctx.fillStyle = "#FFFFFF";
+            ctx.strokeStyle = "#424242";
+            if (c.x == user.x && c.y == user.y) {
+                ctx.fillStyle = "#5882FA";
+            } else if (c.isObstacle) {
+                ctx.fillStyle = "#000000";
+            } else if (intersectingCells.indexOf(c) > -1) {
+                ctx.fillStyle = c.isVisible ? "#F2F2F2" : "#A4A4A4";
+            } else if (!c.isVisible) {
+                ctx.fillStyle = "#848484";
+            }
+            ctx.fillRect(c.x * cellW, c.y * cellH, cellW, cellH);
+            ctx.strokeRect(c.x * cellW, c.y * cellH, cellW, cellH);
+        });
+
+        if (showPolygons) {
+            ctx.strokeStyle = "#084B8A";
+            polylines.forEach(poly => {
+                poly.forEach(line => {
+                    let start = line[0];
+                    let end = line[1];
+                    ctx.beginPath();
+                    ctx.moveTo(start[0], start[1]);
+                    ctx.lineTo(end[0], end[1]);
+                    ctx.stroke();
+                });
+            });
+        }
+    }
+
+    document.onkeyup = (event) => {
+        let x = event.which || event.keyCode;
+        let dir = {
+            37: "left",
+            39: "right",
+            40: "down",
+            38: "up"
+        }[x];
+        if (dir === "right") {
+            user.x += 1;
+        } else if (dir === "left") {
+            user.x -= 1;
+        } else if (dir === "up") {
+            user.y -= 1;
+        } else if (dir === "down") {
+            user.y += 1;
+        }
+        draw();
+    };
+
+    document.getElementById("check").addEventListener("change",() => {
+        showPolygons = !showPolygons;
+        draw();
+    });
+
+    draw();
+};
